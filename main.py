047166e79b3b0f8bd440789930666a2ab4e72ad4
John Francis <john9francis@gmail.com>
2024-02-09 13:55:04 -0700

    Got the basic relaxation done

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # plate 2 at V = -1
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 world = np.zeros([10, 10])
 
@@ -18,4 +19,61 @@ plate2_pos_col = 7
 
 world[range(plate1_pos_row, plate1_pos_row + plate1_height), range(plate1_pos_col, plate1_pos_col + plate1_width)] = -1
 world[range(plate2_pos_row, plate2_pos_row + plate2_height), range(plate2_pos_col, plate2_pos_col + plate2_width)] = 1
-print(world)
+
+
+#print(world)
+# plot
+plt.imshow(world, cmap='viridis')
+plt.colorbar()
+plt.show()
+
+
+# relaxation method:
+rows, columns = world.shape
+difference = 1
+running = True
+
+#while running:
+for i in range(10):
+  # we assume that it's accurate enough, and check later
+  running = False
+
+  for i in range(rows):
+    for j in range(columns):
+        
+      # otherwise, calculate the avg here
+      try:
+        avg = .25 * (world[i-1,j] + world[i+1,j] + world[i,j-1] + world[i,j+1])
+      except IndexError:
+        pass
+      # difference of the old vs. new value
+      difference = abs(avg - world[i,j])
+
+      # if ANY of the differences are too big,
+      # we are going to keep running.
+      if difference > 1e-4:
+        running = True
+
+
+      # reassign this entry to the average.
+      world[i,j] = avg
+
+      # reassign our edges and capacitors
+      
+      # capacitors
+      world[range(plate1_pos_row, plate1_pos_row + plate1_height), range(plate1_pos_col, plate1_pos_col + plate1_width)] = -1
+      world[range(plate2_pos_row, plate2_pos_row + plate2_height), range(plate2_pos_col, plate2_pos_col + plate2_width)] = 1
+
+      # edges
+      world[0, :] = 0
+      world[rows-1, :] = 0
+      world[:, 0] = 0
+      world[:, columns-1] = 0
+
+
+
+#print(world)
+# plot again
+plt.imshow(world, cmap='viridis')
+plt.colorbar()
+plt.show()
